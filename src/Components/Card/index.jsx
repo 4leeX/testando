8b07cb72417a0515d06ef76ebaddeacf53551e6f
alex <alexsandro.sousa12366@gmail.com>
2021-06-10 22:51:57 -0300

    done

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import produtos from '../../produtos';
-import Container from './styled'
+import Container from './styled';
 import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
 import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 import LocalMallIcon from '@material-ui/icons/LocalMall';
@@ -24,49 +24,58 @@ export default function Card() {
   const handleRight = () => {
     let x = scrollX - Math.round(window.innerWidth / 2);
     let listW = len * 180;
-    if ((window.innerWidth - listW) > x) {
-      x = (window.innerWidth - listW) - 60;
+    if (window.innerWidth - listW > x) {
+      x = window.innerWidth - listW - 60;
     }
     setScrollX(x);
   };
 
   return (
     <Container>
-    <div className="movieRow">
-      <div className="movieRow-left" onClick={handleLeft}>
-        <NavigateBeforeIcon style={{ fontSize: 50 }} />
-      </div>
-      <div className="movieRow-right" onClick={handleRight}>
-        <NavigateNextIcon style={{ fontSize: 50 }} />
-      </div>
-      <div className="movieRow-listarea">
-        <div
-          className="movieRow-list"
-          style={{
-            marginLeft: scrollX,
-            width: 999999,
-          }}
-        >
-          {produtos.map((produto, key) => (
-            <div className="movieRow-item" key={key}>
-              <img src={produto.image} alt={produto.name} />
-              <div className="card-info">
-                <h4 className="nome">{produto.name.substring(0, 32)}...</h4>
-                <h4 className="est">{produto.product_condition} Em ótimo estado</h4>
-                <h4 className="precos">R$ {parseFloat(produto.price * 0.91).toFixed(2)} à vista</h4>
-                <h4 className="precos-2"><PaymentIcon style={{ fontSize: 20 }} /> 10x de {produto.price / 10},00 <br />
-                  <span>(R$ {produto.price},00)</span>
-                </h4>
-                <div className="hov">
-                  <button>VER PRODUTO</button>
-                  <div className="bag"><LocalMallIcon style={{ fontSize: 20 }}/> ADICIONAR NA SACOLA</div>
+      <div className="movieRow">
+        <div className="movieRow-left" onClick={handleLeft}>
+          <NavigateBeforeIcon style={{ fontSize: 50 }} />
+        </div>
+        <div className="movieRow-right" onClick={handleRight}>
+          <NavigateNextIcon style={{ fontSize: 50 }} />
+        </div>
+        <div className="movieRow-listarea">
+          <div
+            className="movieRow-list"
+            style={{
+              marginLeft: scrollX,
+              width: 999999,
+            }}
+          >
+            {produtos.map((produto, key) => (
+              <div className="movieRow-item" key={key}>
+                <img src={produto.image} alt={produto.name} />
+                <div className="card-info">
+                  <h4 className="nome">{produto.name.substring(0, 32)}...</h4>
+                  <h4 className="est">
+                    {produto.product_condition} Em ótimo estado
+                  </h4>
+                  <h4 className="precos">
+                    R$ {parseFloat(produto.price * 0.91).toFixed(2)} à vista
+                  </h4>
+                  <h4 className="precos-2">
+                    <PaymentIcon style={{ fontSize: 20 }} /> 10x de{' '}
+                    {produto.price / 10},00 <br />
+                    <span>(R$ {produto.price},00)</span>
+                  </h4>
+                  <div className="hov">
+                    <button>VER PRODUTO</button>
+                    <div className="bag">
+                      <LocalMallIcon style={{ fontSize: 20 }} /> ADICIONAR NA
+                      SACOLA
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
-    </div>
     </Container>
   );
 }
--- src/Components/Card/styled.js
+++ src/Components/Card/styled.js
@@ -1,120 +1,117 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 const Container = styled.section`
-
-.movieRow {
-  margin-bottom: 30px;
-}
-
-.movieRow-listarea {
-  overflow-x: hidden;
-  padding-left: 30px;
-}
-.movieRow-list {
-  transition: all ease 0.5s;
-}
-.movieRow-item {
-  display: inline-block;
-  transform: scale(0.9);
-  width: 180px;
-  cursor: pointer;  
-
-  .hov{
-    opacity: 0;
+  .movieRow {
+    margin-bottom: 30px;
   }
-}
 
-.movieRow-item:hover{
-  transform: scale(1);
-  border-radius: 4px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-  transition: all ease 0.2s;
+  .movieRow-listarea {
+    overflow-x: hidden;
+    padding-left: 30px;
+  }
+  .movieRow-list {
+    transition: all ease 0.5s;
+  }
+  .movieRow-item {
+    display: inline-block;
+    transform: scale(0.9);
+    width: 180px;
+    cursor: pointer;
 
-  .hov{
+    .hov {
+      opacity: 0;
+    }
+  }
+
+  .movieRow-item:hover {
+    transform: scale(1);
+    border-radius: 4px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    transition: all ease 0.2s;
+
+    .hov {
+      opacity: 1;
+    }
+  }
+
+  .movieRow-item img {
+    width: 100%;
+  }
+
+  .movieRow-left,
+  .movieRow-right {
+    position: absolute;
+    width: 40px;
+    height: 500px;
+    z-index: 99;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    overflow: hidden;
+    cursor: pointer;
+    opacity: 0;
+    transition: all ease 0.5s;
+  }
+  .movieRow-left {
+    left: 0;
+  }
+  .movieRow-right {
+    right: 0;
+  }
+  .movieRow:hover .movieRow-left,
+  .movieRow:hover .movieRow-right {
     opacity: 1;
   }
-}
 
-.movieRow-item img {
-  width: 100%;
-}
+  .card-info {
+    text-align: center;
+  }
 
-.movieRow-left,
-.movieRow-right {
-  position: absolute;
-  width: 40px;
-  height: 500px;
-  z-index: 99;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  overflow: hidden;
-  cursor: pointer;
-  opacity: 0;
-  transition: all ease 0.5s;
-}
-.movieRow-left {
-  left: 0;
-}
-.movieRow-right {
-  right: 0;
-}
-.movieRow:hover .movieRow-left,
-.movieRow:hover .movieRow-right
-{
-  opacity: 1;
-}
+  .nome {
+    font-weight: 100;
+  }
 
-.card-info{
-  text-align: center;
-}
+  .precos {
+    font-size: 19px;
+    font-family: serif;
+  }
 
-.nome {
-  font-weight: 100;
-}
+  .precos-2 {
+    color: grey;
+    font-weight: initial;
+    font-size: 17px;
+    text-align: center;
+  }
+  span {
+    font-size: 13px;
+  }
 
-.precos {
-  font-size: 19px;
-  font-family: serif;
-}
+  .est {
+    background-color: orange;
+    opacity: 0.4;
+    color: #b43839;
+    font-weight: bold;
+    border-radius: 50px;
+    font-size: 13px;
+    padding-top: 5px;
+    padding-bottom: 5px;
+  }
 
-.precos-2 {
-  color: grey;
-  font-weight: initial;
-  font-size: 17px;
-  text-align: center;
-}
- span {
-  font-size: 13px;
-}
+  button {
+    background: black;
+    color: white;
+    border: none;
+    margin: 10px;
+    padding: 10px 10px 10px;
+    padding-left: 10px;
+    cursor: pointer;
+    font-size: 12px;
+  }
 
-.est {
-  background-color: orange;
-  opacity: 0.4;
-  color: #b43839;
-  font-weight: bold;
-  border-radius: 50px;
-  font-size: 13px;
-  padding-top: 5px;
-  padding-bottom: 5px;
-}
-
-button{
-  background: black;
-  color: white;
-  border: none;
-  margin: 10px;
-  padding: 10px 10px 10px;
-  padding-left: 10px;
-  cursor: pointer;
-  font-size: 12px;
-}
-
-.bag{
-  padding-top: 10px;
-  font-size: 13px;
-}
-
+  .bag {
+    padding-top: 10px;
+    font-size: 13px;
+  }
 `;
 
 export default Container;
